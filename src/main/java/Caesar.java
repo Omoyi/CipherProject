41b265983e324cc97e5c5294d618c681cd155a8d
Omoyi <urakhel7@gmail.com>
2019-09-06 19:33:55 +0200

    change variable name

--- src/main/java/Caesar.java
+++ src/main/java/Caesar.java
@@ -4,7 +4,8 @@ public class Caesar {
     public static void main(String[] args){
        System.out.println("1. Encryption 2. Decryption Choose(1, 2): ");
        char[] letters = {'A', 'B', 'C','D', 'E', 'F', 'G', 'H', 'I', 'J','K', 'L', 'M', 'N', 'O', 'P', 'Q','R', 'S', 'T', 'U', 'V', 'W', 'X','Y', 'Z'};
-       Scanner myProject = new Scanner(System.in);
-       int choice = myProject.nextInt();
+       Scanner str = new Scanner(System.in);
+       int choice = str.nextInt();
+
     }
 }

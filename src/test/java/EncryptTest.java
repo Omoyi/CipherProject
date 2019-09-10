5c88895ec2d0d8a6f1925c9ecc0f93a15fbb9a9c
Omoyi <urakhel7@gmail.com>
2019-09-10 10:33:40 +0200

    add more tests

--- src/test/java/EncryptTest.java
+++ src/test/java/EncryptTest.java
@@ -1,2 +1,13 @@
+import Cipher.Encryption;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class EncryptTest {
-}
+        @Test
+        public void Encrypt_test_string(){
+            Encryption encryptionTest = new Encryption();
+            String expectedOutput = "Rachel";
+
+            assertEquals(expectedOutput, encryptionTest.Encryption("hi", 4));
+        }
+    }
